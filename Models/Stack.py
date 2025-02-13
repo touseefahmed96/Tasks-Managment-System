01edca5eebefb41ed7e3fa8ba47594c627b56db1
Touseef Ahmad <touseefahmed9669@gmail.com>
2025-02-13 12:32:51 +0500

    Model used in the projects

---
+++ Models/Stack.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack is empty")
+
+    def is_empty(self):
+        return len(self.items) == 0
